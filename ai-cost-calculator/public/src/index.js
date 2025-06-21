9aad1c9960895b24ccc0a182c6ef012a23bc6fe6
Swapnil <stamsepublic@gmail.com>
2025-06-21 17:41:04 -0400

    Add index.

---
+++ ai-cost-calculator/public/src/index.js
@@ -0,0 +1,18 @@
+// ai-cost-calculator/src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/index.css"; // Global CSS from Create React App
+import App from "./App";
+import reportWebVitals from "./reportWebVitals"; // For performance monitoring
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
